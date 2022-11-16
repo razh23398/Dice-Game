e5a6b89b21d1a529efb141ce8829dba1b4a3642d
Raz Hershko <115927546+razh23398@users.noreply.github.com>
2022-11-16 15:54:35 +0200

    First time using JS

---
+++ index.js
@@ -0,0 +1,45 @@
+//Dice number 1
+// Dice 1 pick number
+var randomNumber1 = (Math.floor((Math.random()*6)) + 1)
+// Dice 1 rulls
+if (randomNumber1 === 1){
+  document.querySelector(".img1").setAttribute("src", "images/dice1.png")
+}else if (randomNumber1 === 2){
+  document.querySelector(".img1").setAttribute("src", "images/dice2.png")
+}else if (randomNumber1 === 3){
+  document.querySelector(".img1").setAttribute("src", "images/dice3.png")
+}else if (randomNumber1 === 4){
+  document.querySelector(".img1").setAttribute("src", "images/dice4.png")
+}else if (randomNumber1 === 5){
+  document.querySelector(".img1").setAttribute("src", "images/dice5.png")
+}else if (randomNumber1 === 6){
+  document.querySelector(".img1").setAttribute("src", "images/dice6.png")
+}
+
+//Dice number 2 //
+// Dice 2 pick number
+var randomNumber2 = (Math.floor((Math.random()*6)) + 1)
+// Dice 2 rulls
+if (randomNumber2 === 1){
+  document.querySelector(".img2").setAttribute("src", "images/dice1.png")
+}else if (randomNumber2 === 2){
+  document.querySelector(".img2").setAttribute("src", "images/dice2.png")
+}else if (randomNumber2 === 3){
+  document.querySelector(".img2").setAttribute("src", "images/dice3.png")
+}else if (randomNumber2 === 4){
+  document.querySelector(".img2").setAttribute("src", "images/dice4.png")
+}else if (randomNumber2 === 5){
+  document.querySelector(".img2").setAttribute("src", "images/dice5.png")
+}else if (randomNumber2 === 6){
+  document.querySelector(".img2").setAttribute("src", "images/dice6.png")
+}
+
+
+//  H1 rulls
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Win!"
+}else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Win!🚩"
+}else if (randomNumber1 = randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw!"
+}
